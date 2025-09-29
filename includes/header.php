4b3fdbf95381b286b113c2a8fd04106191483461
Bennett Mikwala <homerunnerb@gmail.com>
2025-09-29 16:26:04 +0200

    final commit for now

--- includes/header.php
+++ includes/header.php
@@ -27,6 +27,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Transaction Dashboard</title>
+    <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
     <link rel="stylesheet" href="css/style.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <style>
